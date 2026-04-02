60d9a05e7ecaec2b8a97c7efd83b2cec9aa65369
KnotPP <pattaraponknot@gmail.com>
2026-04-02 15:53:51 +0700

    config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // ใส่ชื่อ Alias ที่คุณตั้งใน IIS ลงไป (ต้องมี / ปิดหัวท้าย)
+  base: '/test/', 
   plugins: [react()],
 })
